a4fa2a8d0c998b9ee5f86d7debf522199f998d5e
Danielle <danielle.rojas0729@gmail.com>
2025-06-11 11:59:14 +0800

    Added user attempt for quizzes

--- server/db/models/Lesson.mjs
+++ server/db/models/Lesson.mjs
@@ -3,15 +3,16 @@ import mongoose from 'mongoose';
 const stepSchema = new mongoose.Schema({
   type: {
     type: String,
-    enum: ['text', 'video', 'quiz', 'code', 'text-video', 'text-code'], // Added 'text-code'
+    enum: ['text', 'video', 'quiz', 'code', 'text-video', 'text-code'],
     required: true,
   },
-  content: String, // Code snippet, video URL, or text content
-  text: String,    // Explanatory text (used for text-video and text-code)
-  language: String,
-  question: String,
-  options: [String],
-  answer: String,
+  content: String,       // Code snippet, video URL, or text content
+  text: String,          // Explanatory text (used for text-video and text-code)
+  language: String,      // For code snippets
+  question: String,      // For quizzes
+  options: [String],     // For quizzes
+  answer: String,        // For quizzes
+  explanation: String    // 2-try quiz feedback
 });
 
 const lessonSchema = new mongoose.Schema({
