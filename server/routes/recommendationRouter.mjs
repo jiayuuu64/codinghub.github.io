90e100ed418f68759d5f25bb194431fee0ad81cc
jiayuuu64 <2305136J@student.tp.edu.sg>
2025-07-16 17:27:48 +0800

    Fixing the Recommended For You section whereby the final quiz of the course will be display there

--- server/routes/recommendationRouter.mjs
+++ server/routes/recommendationRouter.mjs
@@ -1,6 +1,8 @@
 import express from 'express';
 import User from '../db/models/User.mjs';
 
+const router = express.Router();
+
 router.get('/recommendations', async (req, res) => {
   const { email } = req.query;
   if (!email) return res.status(400).json({ error: 'Missing email' });
