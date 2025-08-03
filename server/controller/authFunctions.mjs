41ecc98d339d880d7786f69bcc09c70b924cc294
jiayuuu64 <2305136J@student.tp.edu.sg>
2025-08-03 16:34:17 +0800

    Adding progressRouter.mjs

--- server/controller/authFunctions.mjs
+++ server/controller/authFunctions.mjs
@@ -315,15 +315,12 @@ export const completeLesson = async (req, res) => {
   const { email } = req.query;
   const { lessonId } = req.body;
 
-console.log('✅ Email received:', email);
-  console.log('✅ Lesson ID received:', lessonId);
-
   if (!email) {
     return res.status(400).json({ message: 'Email is required.' });
   }
 
   try {
-    const user = await User.findOne({ email });
+    const user = await User.findOne({ email: email.toLowerCase() });
     if (!user) return res.status(404).json({ message: 'User not found' });
 
     const courseIdStr = courseId.toString(); // Ensure string comparison
